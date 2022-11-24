6f75a6b85454a0353a496760001c688424c9415b
Nicolas Muckensturm <Nicolas Muckensturm@nicolas.muckensturm@etu.univ-amu.fr>
2022-11-24 12:28:02 +0100

    add layout

---
+++ front/components/Layout/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header: React.FC = () => {
+
+    return (
+        <header>
+
+            $$$$
+
+        </header>
+    )
+
+}
+
+export default Header;
---
+++ front/components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+type LayoutProps = {
+    children: JSX.Element
+}
+
+const Layout: React.FC<LayoutProps> = ({children}) => {
+
+    return (
+        <>
+            <Header />
+                {children}
+            <Footer />
+        </>
+    )
+
+}
+
+export default Layout;
