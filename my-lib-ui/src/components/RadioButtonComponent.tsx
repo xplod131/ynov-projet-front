7020eb9a189d19c6d83d0f13cfb2648fd234d7fd
intracable78 <matteo.tomasi@ynov.com>
2022-11-25 11:13:12 +0100

    style

--- my-lib-ui/src/components/RadioButtonComponent.tsx
+++ my-lib-ui/src/components/RadioButtonComponent.tsx
@@ -6,7 +6,22 @@ type Props = React.DetailedHTMLProps<
 > & { label: string };
 
 const RadioButtonComponent: React.FC<Props> = (props) => {
-  return <div></div>;
+  return (
+    <div className="my-lib-ui-radio-container">
+      <legend>Je suis : </legend><br />
+      <div className="container-select-radio">
+        <div>
+          <input type="radio" id="entreprise" name="unique" value="Une entreprise" />
+          <label htmlFor="entreprise">Une entreprise</label>
+        </div>
+
+        <div>
+          <input type="radio" id="particulier" name="unique" value="Un particulier" />
+          <label htmlFor="particulier">Un particulier</label>
+        </div>
+      </div>
+
+    </div>);
 };
 
 export default RadioButtonComponent;
