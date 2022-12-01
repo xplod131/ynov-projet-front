00f22d852868c7f92bbc025c0aec59c5fb5eb2bf
Nicolas Muckensturm <Nicolas Muckensturm@nicolas.muckensturm@etu.univ-amu.fr>
2022-12-01 14:06:56 +0100

    add confirm inscription page

---
+++ front/components/ConfirmRegister.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from '../styles/ConfirmRegister.module.css';
+import Image from "next/image";
+import Apple from '../img/Apple.png';
+import Googleplay from '../img/GooglePlay.svg';
+
+const ConfirmRegister: React.FC = () => {
+
+    return (
+        <div className={styles.confirmContainer}>
+            <p>Votre demande d’inscription a bien été prise en compte.<br /> Vous allez recevoir une confirmation par mail, en attendant vous pouvez télécharger l’application.</p>
+
+            <div className={styles.box}>
+                <Image src={Apple} className={styles.imageBoxApple} alt={'gfdgfd'}/>
+                <Image src={Googleplay} className={styles.imageBoxGooglePlay} alt={'gfdgfd'}/>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default ConfirmRegister;
